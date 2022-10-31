c5fab3509f4abd4ab164d013bcdd7fc757a89c9b
cnshwlmiles <73220443+cnshwlmiles@users.noreply.github.com>
2022-10-31 19:06:53 +0800

    1031進度

--- 0台大系統資訊訓練班/0玉米專家系統/2022_10_05_玉米專家系統動態/js/gdd_calc.js
+++ 0台大系統資訊訓練班/0玉米專家系統/2022_10_05_玉米專家系統動態/js/gdd_calc.js
@@ -1,3 +1,4 @@
+// 取得表單資訊時間  TODO:  氣象站代碼沒有自動化
 function get_form_data(){
     var data_list = {};
     $(".form-box .form_data").each(function(num, dom) {
@@ -11,3 +12,29 @@ function get_form_data(){
     // console.log(data_list);
     return data_list;    
 }
+
+// 對檔案算積溫
+// const tempData = require('./虎尾_2016_2021.json');
+// console.log(tempData)
+
+function get_temp_data() {
+    $.ajax({
+        type: "GET",
+        url:  global.controller + '/虎尾_2016_2021.json',
+        dataType: "JSON",
+        success: function (data_response) {
+            temp_list = data_response;
+            console.log(temp_list);
+            // $.each(data_response, function (city_name, area) { 
+            //      $("#city_choose").append("<option>" + city_name + "</option>");
+            // });           
+        },
+        error:function(){
+            alert('請求失敗')
+        }
+    });
+}
+
+// fetch("..//gdd_calculation/get_city_list.json")
+//     .then((response) => response.json())
+//     // .then((json) => console.log(json));
